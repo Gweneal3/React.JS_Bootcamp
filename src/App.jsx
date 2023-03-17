d5747a9217b64f8c2526ef54db7cd42d1f4b6271
Lai Ke Wei <77392505+Gweneal3@users.noreply.github.com>
2023-03-17 22:46:53 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+
+const react = require("react");
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import App from "./App";
+const react = require("react");
+const reactDOM = require("react-dom");
+
+reactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
+
+// var p = document.createElement("p");
+// p.innerHTML = "Hello World!!!";
+// document.getElementById("root").appendChild(p);
